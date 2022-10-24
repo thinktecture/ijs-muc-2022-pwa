df09c4d97b74f6457ba4a6fbdf0e3a8a1edaf2d6
Christian Liebel <christian.liebel@thinktecture.com>
2022-10-24 09:45:16 +0200

    Lab #2

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
 import {bresenhamLine, getImage, toBlob} from "./helpers.js";
 
 const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = 'black';
